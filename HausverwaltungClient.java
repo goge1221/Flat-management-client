a304cfd61f6bcaacff716fb12f7b7e950e9a80cd
goge1221 <75140192+goge1221@users.noreply.github.com>
2021-10-23 19:44:41 +0200

    Initial commit

---
+++ HausverwaltungClient.java
@@ -0,0 +1,7 @@
+public class HausverwaltungClient {
+
+	public static void main(String[] args) {	
+
+	}
+	
+}
---
+++ HausverwaltungSerializationDAO.java
@@ -0,0 +1,3 @@
+public class HausverwaltungSerializationDAO implements HausverwaltungDAO {
+
+}
